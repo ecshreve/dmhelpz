5df3ebf90ccb384efb1d14a2f911a8d56e002147
Eric Shreve <ecshreve13@gmail.com>
2021-08-21 23:37:25 -0700

    dmhelpz: update main func to use client

--- cmd/dmhelpz/main.go
+++ cmd/dmhelpz/main.go
@@ -1,7 +1,25 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"log"
+	"net/http"
+
+	"github.com/Yamashou/gqlgenc/client"
+	"github.com/ecshreve/dmhelpz/pkg/gql/gen"
+	"github.com/kr/pretty"
+)
 
 func main() {
-	fmt.Println("hello world")
+	ctx := context.Background()
+
+	client := &gen.Client{
+		Client: client.NewClient(http.DefaultClient, "https://www.dnd5eapi.co/graphql"),
+	}
+
+	m, err := client.GetMonster(ctx, "Aboleth")
+	if err != nil {
+		log.Fatal(err)
+	}
+	pretty.Print(m)
 }
